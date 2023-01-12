f9c8baf9bcc0435a71d0b9fb0d73223b2086781a
connor young <connor675756@gmail.com>
2023-01-12 22:44:43 +0000

    First Commit

---
+++ src/main/java/com/github/readutf/Hermes.java
@@ -0,0 +1,12 @@
+package com.github.readutf;
+
+import java.util.HashMap;
+
+public class Hermes {
+
+    private static Hermes instance;
+
+    public Hermes() {
+        instance = this;
+    }
+}
